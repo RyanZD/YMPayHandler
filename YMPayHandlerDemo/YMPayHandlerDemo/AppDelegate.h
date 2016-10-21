ae628d0e0c0a18a043544d687fc5181d37d78b0c
张锐 <zhangrui02@xywy.com>
2016-10-21 11:30:15 +0800

    demo init

---
+++ YMPayHandlerDemo/YMPayHandlerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMPayHandlerDemo
+//
+//  Created by 张锐 on 2016/10/21.
+//  Copyright © 2016年 张锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YMPayHandlerDemo/YMPayHandlerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMPayHandlerDemo
+//
+//  Created by 张锐 on 2016/10/21.
+//  Copyright © 2016年 张锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
